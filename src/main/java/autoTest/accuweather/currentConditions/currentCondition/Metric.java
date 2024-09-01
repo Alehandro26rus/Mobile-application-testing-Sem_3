056ad0506ea2635dafce235f226802a9920083ae
Alehandro_26rus <skash96@yandex.ru>
2024-09-01 22:02:30 +0300

    Added other changes

--- src/main/java/autoTest/accuweather/currentConditions/currentCondition/Metric.java
+++ src/main/java/autoTest/accuweather/currentConditions/currentCondition/Metric.java
@@ -1,16 +1,15 @@
 
 package autoTest.accuweather.currentConditions;
 
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-    "Value",
-    "Unit",
-    "UnitType"
+        "Value",
+        "Unit",
+        "UnitType"
 })
 
 public class Metric {
